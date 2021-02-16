983a5896afb25cee0d9f4d930d880d5314f7f917
Lydia Zhang <mengyao1227@gmail.com>
2021-02-16 14:32:36 +1100

    AW-8 add findByEmail method in UserRespository

--- src/main/java/com/asyncworking/repositories/UserRepository.java
+++ src/main/java/com/asyncworking/repositories/UserRepository.java
@@ -1,9 +1,10 @@
 package com.asyncworking.repositories;
 
-import com.asyncworking.models.UserEntity;
+import com.asyncworking.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
 }
