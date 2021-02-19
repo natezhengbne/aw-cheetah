7ce8315dafc6677ee04962bb78464689276e7813
AisinAllin <AisinAllin@gmail.com>
2021-02-19 11:46:26 +1100

    AW-10 create password

--- src/main/resources/db/migration/V1__USER_TABLE.sql
+++ src/main/resources/db/migration/V1__USER_TABLE.sql
@@ -1,14 +1,14 @@
-CREATE SCHEMA if not exists aw-cheetah;
+CREATE SCHEMA if not exists awcheetah;
 
 DROP TABLE IF EXISTS "user";
-CREATE TYPE status AS ENUM('Unverified', 'Activated', 'Cancelled');
+CREATE TYPE status AS ENUM('UNVERIFIED', 'ACTIVATED', 'CANCELLED');
 CREATE TABLE "user" (
     "id" BIGINT PRIMARY KEY,
     "email" VARCHAR(255) UNIQUE NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     "title" VARCHAR(255) NOT NULL,
     "password" CHAR(64) NOT NULL,
-    "status" status default 'Unverified',
+    "status" status default 'UNVERIFIED',
     "created_time" TIMESTAMP WITH TIME ZONE NOT NULL,
     "updated_time" TIMESTAMP WITH TIME ZONE NOT NULL
 );
--- src/test/resources/USER_TEST_DATA.sql
+++ src/test/resources/USER_TEST_DATA.sql
@@ -1,5 +1,5 @@
-INSERT INTO "user" VALUES (DEFAULT, 'chui@asyncworking.com', 'Chui', 'Tech Lead', '', 'Activated', '2021-02-15 16:31:50', '2021-02-17 16:33:44');
-INSERT INTO "user" VALUES (DEFAULT, 'lawrence@asyncworking.com', 'Lawrence', 'Backend Developer', 'law1928', 'Cancelled', '2021-02-16 16:33:50', '2021-02-18 16:37:44');
-INSERT INTO "user" VALUES (DEFAULT, 'lengary@asyncworking.com', 'Lengary', 'Frontend Developer', 'len3746', 'Unverified', '2021-02-19 18:31:50', '2021-02-20 16:30:44');
-INSERT INTO "user" VALUES (DEFAULT, 'robert@asyncworking.com', 'Robert', 'Frontend Developer', '$2y$10$GnVxr6ijNntAFXSdifoiJuOGzMsVVMeRSRGYsR4LFDrweVr3IIgWC', 'Activated', '2021-02-17 10:31:50', '2021-02-21 20:33:44');
-INSERT INTO "user" VALUES (DEFAULT, 'yexin@asyncworking.com', 'Yexin', 'Business Analyst', '$2y$10$kjtZN/uVMmbvYksOvRHCPeyKjVrmOSdSWYNrR9MEb/hTCFzB58EPi', 'Cancelled', '2021-02-20 08:31:50', '2021-02-28 22:23:44');
+INSERT INTO "user" VALUES (DEFAULT, 'chui@asyncworking.com', 'Chui', 'Tech Lead', '', 'ACTIVATED', '2021-02-15 16:31:50', '2021-02-17 16:33:44');
+INSERT INTO "user" VALUES (DEFAULT, 'lawrence@asyncworking.com', 'Lawrence', 'Backend Developer', 'law1928', 'CANCELLED', '2021-02-16 16:33:50', '2021-02-18 16:37:44');
+INSERT INTO "user" VALUES (DEFAULT, 'lengary@asyncworking.com', 'Lengary', 'Frontend Developer', 'len3746', 'UNVERIFIED', '2021-02-19 18:31:50', '2021-02-20 16:30:44');
+INSERT INTO "user" VALUES (DEFAULT, 'robert@asyncworking.com', 'Robert', 'Frontend Developer', '$2y$10$GnVxr6ijNntAFXSdifoiJuOGzMsVVMeRSRGYsR4LFDrweVr3IIgWC', 'ACTIVATED', '2021-02-17 10:31:50', '2021-02-21 20:33:44');
+INSERT INTO "user" VALUES (DEFAULT, 'yexin@asyncworking.com', 'Yexin', 'Business Analyst', '$2y$10$kjtZN/uVMmbvYksOvRHCPeyKjVrmOSdSWYNrR9MEb/hTCFzB58EPi', 'CANCELLED', '2021-02-20 08:31:50', '2021-02-28 22:23:44');
