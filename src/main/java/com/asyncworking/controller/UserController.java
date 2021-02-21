2dfbb4a630907a74ce7215b11c3baaf60219449b
AisinAllin <AisinAllin@gmail.com>
2021-02-21 13:00:11 +1100

    AW-10 connect end & front

--- src/main/java/com/asyncworking/controller/UserController.java
+++ src/main/java/com/asyncworking/controller/UserController.java
@@ -7,13 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/signup")
+//@RequestMapping("/signup")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping()
+    @PostMapping("/signup")
     public ResponseEntity<UserInfoDto> createUser(@RequestBody UserInfoDto userInfoDto) {
         UserInfoDto userInfoDtoPassword = userService.createUser(userInfoDto);
         return ResponseEntity.ok(userInfoDtoPassword);
