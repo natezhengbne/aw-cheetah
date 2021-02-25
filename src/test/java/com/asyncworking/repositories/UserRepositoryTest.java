d0ae0c8f6fc70bb65714393ca1056303b2da0782
Steven Wang <skykk0128@gmail.com>
2021-02-25 14:09:47 +1100

    AW-9 Fix issues regarding tests

--- src/test/java/com/asyncworking/repositories/UserRepositoryTest.java
+++ src/test/java/com/asyncworking/repositories/UserRepositoryTest.java
@@ -2,12 +2,14 @@ package com.asyncworking.repositories;
 
 import com.asyncworking.models.Status;
 import com.asyncworking.models.UserEntity;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 import java.time.OffsetDateTime;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class UserRepositoryTest {
 
 	@Autowired
-	private  UserRepository repository;
+	private  UserRepository userRepository;
 
 	@Test
 	@Transactional
@@ -31,8 +33,40 @@ class UserRepositoryTest {
 				.updatedTime(OffsetDateTime.now())
 				.build();
 
-		UserEntity saved = repository.save(user);
+		UserEntity saved = userRepository.save(user);
 		assertEquals("email", saved.getEmail());
 		assertThat(saved.getId() > 0);
+	}
+
+	@Test
+	public void shouldAddUserEntityIntoDBSuccessfullyGivenProperUserEntity() {
+		UserEntity userEntity = UserEntity.builder()
+				.id(1L)
+				.createdTime(OffsetDateTime.now())
+				.email("skykk0128@gmail.com")
+				.name("Steven")
+				.password("password")
+				.status(Status.UNVERIFIED)
+				.title("Developer")
+				.updatedTime(OffsetDateTime.now()).build();
+		UserEntity returnedUserEntity = userRepository.save(userEntity);
+		Assertions.assertEquals("Steven", returnedUserEntity.getName());
+		Assertions.assertEquals("skykk0128@gmail.com", returnedUserEntity.getEmail());
+	}
+
+	@Test
+	public void shouldFindUserByEmail() {
+		UserEntity userEntity = UserEntity.builder()
+				.id(1L)
+				.createdTime(OffsetDateTime.now())
+				.email("skykk0128@gmail.com")
+				.name("Steven")
+				.password("password")
+				.status(Status.UNVERIFIED)
+				.title("Developer")
+				.updatedTime(OffsetDateTime.now()).build();
+		Optional<UserEntity> returnedUserEntity = userRepository
+				.findByEmail(userEntity.getEmail());
+		Assertions.assertTrue(returnedUserEntity.isEmpty());
 	}
 }
