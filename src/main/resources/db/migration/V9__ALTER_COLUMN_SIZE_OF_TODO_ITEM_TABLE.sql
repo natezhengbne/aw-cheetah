ae8fae5adc2e90506200a40e3e17c390a6a4c78c
jiahuiyang0@gmail.com <jiahuiyang0@gmail.com>
2021-05-29 10:53:22 +1000

    AW-38 altered column size of table todo_item

---
+++ src/main/resources/db/migration/V9__ALTER_COLUMN_SIZE_OF_TODO_ITEM_TABLE.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "todo_item" ALTER COLUMN "description" TYPE VARCHAR(512);
+ALTER TABLE "todo_item" ALTER COLUMN "doc_url" TYPE VARCHAR(512);
+ALTER TABLE "todo_item" ALTER COLUMN "event_doc_url" TYPE VARCHAR(512);
+ALTER TABLE "todo_item" ALTER COLUMN "content" TYPE VARCHAR(10000);
