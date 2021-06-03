0c78cb5312e6eeecb13b68ab41031b58811ebaaa
Weixin Qi <s4590288@gmail.com>
2021-06-03 21:00:11 +1000

    AW-28 AW-28: delete the useless /t and /b

--- src/main/java/com/asyncworking/repositories/UserRepository.java
+++ src/main/java/com/asyncworking/repositories/UserRepository.java
@@ -1,20 +1,17 @@
 package com.asyncworking.repositories;
 
-import com.asyncworking.models.IEmployeeInfo;
 import com.asyncworking.models.Employee;
+import com.asyncworking.models.IEmployeeInfo;
 import com.asyncworking.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
-
-import static java.time.ZoneOffset.UTC;
 
 @Repository
 @EnableJpaRepositories
@@ -59,14 +56,14 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     List<IEmployeeInfo> findAllMembersByProjectId(@Param("id") Long id);
 
     @Query(nativeQuery = true,
-            value = "SELECT u.id, u.name, u.email, userinfo.title FROM (\n" +
-                    "    SELECT * FROM awcheetah.company_user cu \n" +
-                    "\t\tWHERE cu.user_id NOT IN (\n" +
-                    "\t\t\tSELECT projectu.user_id FROM (\n" +
-                    "\t\t\t\tSELECT * FROM awcheetah.project_user pu \n" +
-                    "\t\t\t\t\tWHERE pu.project_id = :projectId) AS projectu) \n" +
-                    "    AND cu.company_id = :companyId) AS userinfo \n" +
-                    "LEFT JOIN awcheetah.user_info u ON u.id = userinfo.user_id AND u.status = 'ACTIVATED'\n" +
+            value = "SELECT u.id, u.name, u.email, userinfo.title FROM ( " +
+                    "SELECT * FROM awcheetah.company_user cu " +
+                    "WHERE cu.user_id NOT IN ( " +
+                    "SELECT projectu.user_id FROM ( " +
+                    "SELECT * FROM awcheetah.project_user pu " +
+                    "WHERE pu.project_id = :projectId) AS projectu) " +
+                    "AND cu.company_id = :companyId) AS userinfo " +
+                    "LEFT JOIN awcheetah.user_info u ON u.id = userinfo.user_id AND u.status = 'ACTIVATED' " +
                     "ORDER BY u.name;")
     List<IEmployeeInfo> findNonMembersEmployeesByCompanyAndProjectId(@Param("companyId") Long companyId,
                                                                     @Param("projectId") Long projectId);
