e5d75b355f94a2030c9c94a01909be9f80562411
jiangcan <can774899203@gmail.com>
2021-08-11 23:41:22 +1000

    AW-98 AW98: add new EmailResultListener class

---
+++ src/main/java/com/asyncworking/controllers/EmailResultListener.java
@@ -0,0 +1,26 @@
+package com.asyncworking.controllers;
+
+import com.asyncworking.services.UserService;
+import io.awspring.cloud.messaging.listener.annotation.SqsListener;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@ConditionalOnProperty(value = "sqs.enable",
+    havingValue = "true",
+    matchIfMissing = true)
+public class EmailResultListener {
+
+    private final UserService userService;
+
+    @SqsListener(value = "${cloud.aws.sqs.incoming-queue.name}")
+    public void loadMessagesFromQueue(String email) {
+        log.info("from sqs: " + email);
+        userService.updateEmailSent(email);
+    }
+}
