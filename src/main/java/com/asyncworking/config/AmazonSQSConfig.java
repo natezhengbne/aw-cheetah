1eaba210ce22e3213149cebc41500b7e732da3f6
johnwu94 <johnwu94@gmail.com>
2021-08-12 16:40:19 +1000

    AW-99  updated keywords in  application.yml

--- src/main/java/com/asyncworking/config/AmazonSQSConfig.java
+++ src/main/java/com/asyncworking/config/AmazonSQSConfig.java
@@ -14,10 +14,10 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AmazonSQSConfig {
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.accesskey}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secretkey}")
     private String secretKey;
 
     @Bean
