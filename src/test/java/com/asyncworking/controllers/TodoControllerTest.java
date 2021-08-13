3471d93d07835d208b32964f54f1dad461f9628f
AisinAllin <AisinAllin@gmail.com>
2021-08-13 16:56:35 +1000

    AW-39 AW-39: fix unit test

--- src/test/java/com/asyncworking/controllers/TodoControllerTest.java
+++ src/test/java/com/asyncworking/controllers/TodoControllerTest.java
@@ -169,7 +169,7 @@ class TodoControllerTest {
         when(todoService.findAssignedPeople(1L, 1L))
                 .thenReturn(List.of(assignedPeopleGetDto, assignedPeopleGetDto2));
 
-        mockMvc.perform(get("/projects/1/todoitems/1/assignedPeoples"))
+        mockMvc.perform(get("/projects/1/todoitems/1/assignees"))
                 .andExpect(status().isOk());
     }
 }
