e217b19e54d751bae2927e3f28a08a21bd20fb11
Tron930123 <jiahuiyang0@gmail.com>
2021-08-17 17:48:39 +1000

    AW-98 fixed local index and environment

--- src/main/java/com/asyncworking/controllers/EmailResultListener.java
+++ src/main/java/com/asyncworking/controllers/EmailResultListener.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 
 
 @Component
