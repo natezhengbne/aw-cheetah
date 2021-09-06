e07ee00336917b934d0140a48241b1b597cd5db6
yuanningd <yuanningd@gmail.com>
2021-09-07 02:44:20 +1000

    AW-158: Remove unnecessary annotations

--- src/main/java/com/asyncworking/jwt/JwtController.java
+++ src/main/java/com/asyncworking/jwt/JwtController.java
@@ -1,9 +1,7 @@
 package com.asyncworking.jwt;
 
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +14,6 @@ public class JwtController {
 
     private final JwtService jwtService;
 
-    @SneakyThrows
     @GetMapping
     public ResponseEntity<JwtDto> refreshToken(@RequestHeader("Authorization") String auth) {
         return ResponseEntity.ok(jwtService.refreshJwtToken(auth));
