02e00658e6ddc550e138bf21873fb26006bf665b
AisinAllin <AisinAllin@gmail.com>
2022-01-23 12:57:24 +0800

    AW193/EMPLOYEES-CAN-SEE-CONTRIBUTION-ACTIVITIES AW-193: update Contribution Service Function

--- src/main/java/com/asyncworking/services/ContributionService.java
+++ src/main/java/com/asyncworking/services/ContributionService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import java.time.DayOfWeek;
 import java.time.OffsetDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,9 +41,7 @@ public class ContributionService {
         OffsetDateTime endDate = start.plusDays(6).withHour(23).withMinute(59).withSecond(59);
         List<TodoItem> completedTodoItems2 = todoItemRepository
                 .findByCompanyIdAndSubscribersIdsIsContainingAndCompletedTimeBetween(companyId, userId.toString(), start, endDate);
-
-        Map<DayOfWeek, List<ContributionActivitiesDto>> oneWeekCompletedTodoItemsMap = getDayTask(completedTodoItems2);
-        return oneWeekCompletedTodoItemsMap;
+        return getDayTask(completedTodoItems2);
     }
 /*
     public Map<DayOfWeek, List<ContributionActivitiesDto>> findOneWeekCompletedTodoItemsList(Long companyId, Long userId) {
@@ -68,40 +65,20 @@ public class ContributionService {
         OffsetDateTime today = OffsetDateTime.now().truncatedTo(ChronoUnit.HOURS);
         OffsetDateTime startDateOfWeek = today.minusDays
                 (today.getDayOfWeek() == DayOfWeek.SUNDAY ? 0 : today.getDayOfWeek().getValue());
-        OffsetDateTime start = startDateOfWeek.withHour(0).withMinute(0).withSecond(0);
-        return start;
+        return startDateOfWeek.withHour(0).withMinute(0).withSecond(0);
     }
 
     private Map<DayOfWeek, List<ContributionActivitiesDto>> getDayTask(List<TodoItem> completedTodoItems2) {
-        List<ContributionActivitiesDto> mondayActivities = new ArrayList<>();
-        List<ContributionActivitiesDto> tuesdayActivities = new ArrayList<>();
-        List<ContributionActivitiesDto> wednesdayActivities = new ArrayList<>();
-        List<ContributionActivitiesDto> thursdayActivities = new ArrayList<>();
-        List<ContributionActivitiesDto> fridayActivities = new ArrayList<>();
-        List<ContributionActivitiesDto> saturdayActivities = new ArrayList<>();
-        List<ContributionActivitiesDto> sundayActivities = new ArrayList<>();
         Map<DayOfWeek, List<ContributionActivitiesDto>> oneWeekCompletedTodoItemsMap = new LinkedHashMap<>();
-        sundayActivities = completedTodoItems2.stream().filter(completeItem -> completeItem.getCompletedTime().getDayOfWeek().equals(DayOfWeek.SUNDAY))
-                .map(sundayItem -> TodoMapper.mapContributionActivitiesDto(sundayItem)).collect(Collectors.toList());
-        oneWeekCompletedTodoItemsMap.put(DayOfWeek.SUNDAY, sundayActivities);
-        mondayActivities = completedTodoItems2.stream().filter(completeItem -> completeItem.getCompletedTime().getDayOfWeek().equals(DayOfWeek.MONDAY))
-                .map(mondayItem -> TodoMapper.mapContributionActivitiesDto(mondayItem)).collect(Collectors.toList());
-        oneWeekCompletedTodoItemsMap.put(DayOfWeek.MONDAY, mondayActivities);
-        tuesdayActivities = completedTodoItems2.stream().filter(completeItem -> completeItem.getCompletedTime().getDayOfWeek().equals(DayOfWeek.TUESDAY))
-                .map(tuesdayItem -> TodoMapper.mapContributionActivitiesDto(tuesdayItem)).collect(Collectors.toList());
-        oneWeekCompletedTodoItemsMap.put(DayOfWeek.TUESDAY, tuesdayActivities);
-        wednesdayActivities = completedTodoItems2.stream().filter(completeItem -> completeItem.getCompletedTime().getDayOfWeek().equals(DayOfWeek.WEDNESDAY))
-                .map(wednesdayItem -> TodoMapper.mapContributionActivitiesDto(wednesdayItem)).collect(Collectors.toList());
-        oneWeekCompletedTodoItemsMap.put(DayOfWeek.WEDNESDAY, wednesdayActivities);
-        thursdayActivities = completedTodoItems2.stream().filter(completeItem -> completeItem.getCompletedTime().getDayOfWeek().equals(DayOfWeek.THURSDAY))
-                .map(thursdayItem -> TodoMapper.mapContributionActivitiesDto(thursdayItem)).collect(Collectors.toList());
-        oneWeekCompletedTodoItemsMap.put(DayOfWeek.THURSDAY, thursdayActivities);
-        fridayActivities = completedTodoItems2.stream().filter(completeItem -> completeItem.getCompletedTime().getDayOfWeek().equals(DayOfWeek.FRIDAY))
-                .map(fridayItem -> TodoMapper.mapContributionActivitiesDto(fridayItem)).collect(Collectors.toList());
-        oneWeekCompletedTodoItemsMap.put(DayOfWeek.FRIDAY, fridayActivities);
-        saturdayActivities = completedTodoItems2.stream().filter(completeItem -> completeItem.getCompletedTime().getDayOfWeek().equals(DayOfWeek.SATURDAY))
-                .map(saturdayItem -> TodoMapper.mapContributionActivitiesDto(saturdayItem)).collect(Collectors.toList());
-        oneWeekCompletedTodoItemsMap.put(DayOfWeek.SATURDAY, saturdayActivities);
+        List<DayOfWeek> dayList = List.of(
+                DayOfWeek.SUNDAY, DayOfWeek.MONDAY, DayOfWeek.TUESDAY,
+                DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY, DayOfWeek.SATURDAY);
+        dayList.forEach(currentDay -> oneWeekCompletedTodoItemsMap.put(currentDay, filterListByDate(completedTodoItems2, currentDay)));
         return oneWeekCompletedTodoItemsMap;
+    }
+
+    private List<ContributionActivitiesDto> filterListByDate(List<TodoItem> completedTodoItems, DayOfWeek dayOfWeek) {
+        return completedTodoItems.stream().filter(completeItem -> completeItem.getCompletedTime().getDayOfWeek().equals(dayOfWeek))
+                .map(TodoMapper::mapContributionActivitiesDto).collect(Collectors.toList());
     }
 }
