159884b1a7b6b3215d51eb9fcd47f276f617195d
eric-0815 <67636157+eric-0815@users.noreply.github.com>
2022-04-27 20:08:16 +1000

    AW-266: improve import

--- src/main/java/com/asyncworking/dtos/todoitem/TodoItemPutDto.java
+++ src/main/java/com/asyncworking/dtos/todoitem/TodoItemPutDto.java
@@ -1,6 +1,11 @@
 package com.asyncworking.dtos.todoitem;
 
-import lombok.*;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 
